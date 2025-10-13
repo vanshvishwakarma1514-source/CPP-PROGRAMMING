7354cd7248b0ed44cedbc0f24d0de53888afb64a
vansh vishwakarma <vanshvishwakarma1514@gmail.com>
2025-10-13 10:39:31 +0530

    msg

---
+++ functions1.cpp
@@ -0,0 +1,39 @@
+
+//  
+
+
+// #include<iostream>
+// using namespace std;
+
+// class One
+// {
+//     friend class Two;
+//     int a = 24;
+//     public:
+//     void show()
+//     {
+//         cout <<"this is class one"<<endl;
+//     }
+// };
+// class Two
+// {
+//     friend class One;
+//     public : 
+//     void show2()
+//     {
+//         cout << "And this is class Two"<<endl;
+//     }
+
+// void accessOneprivate(One &obj)
+// {
+//     cout<<"Accessing data of  base class"<<obj.a<<endl;
+// }
+
+// };
+// int main()
+// {
+// One O1;
+// Two O2; 
+// O2.accessOneprivate(O1);
+// }
+

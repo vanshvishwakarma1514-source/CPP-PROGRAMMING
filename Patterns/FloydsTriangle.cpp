3597d38edf47638e99160a17eafbab264649384e
vansh vishwakarma <vanshvishwakarma1514@gmail.com>
2025-10-27 10:22:12 +0530

    New Commit 2710

--- Patterns/FloydsTriangle.cpp
+++ Patterns/FloydsTriangle.cpp
@@ -8,13 +8,14 @@ int main()
     int m;
     cout<<"enter a column :";
     cin>>m;
+    int a ;
+    a = 0;
+    a++;
     for(int i=1;i<=n;i++)
     {
        for(int j=1;j<=i;j++) 
        {
-        
-        cout<<2*j-1<<" ";
-        
+        cout<<a++<<" ";
        }
        cout<<endl;
     }
